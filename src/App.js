2fe4c7b7aa8a7f09837ca9a3c10b56d43d15ed62
siratl <elishubax@gmail.com>
2019-01-09 15:08:38 -0800

    Created TodoList to render in browser for visual purposes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
+
+
+
+const todos = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -7,7 +23,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <TodoList todos={todos}/>
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,15 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+
+const TodoList = props => {
+    return (
+        <div>
+            {props.todos.map(todo => {
+                return <div>{todo.task}</div>;
+            })}
+        </div>
+    );
+};
+
+export default TodoList;
